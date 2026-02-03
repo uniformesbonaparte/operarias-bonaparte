f7483305bacc4a6e003b52a4974514fa02cf007b
uniformesbonaparte <bonachonpachon@gmail.com>
2026-02-03 10:34:54 -0600

    Add files via upload

--- produccion_nomina_app.js
+++ produccion_nomina_app.js
@@ -7,7 +7,8 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(express.json());
 
